7c434895c77fc70eda3f5a5228ea073bb8a4e5bd
priscarabelli <125477480+priscarabelli@users.noreply.github.com>
2023-07-20 13:19:41 +0100

    added axios and integrated dictionary API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,13 +1,21 @@
 import React, {useState} from "react";
 import "./Dictionary.css"; 
+import axios from "axios"; 
 
 export default function Dictionary(){
     let [Keyword, setKeyword] = useState(""); 
 
-function search(event){
-    event.preventDefault();
-    alert(`Searching for ${Keyword} definition`);
+function handleResponse(response){
+   console.log(response) 
+}
 
+
+function search(event){
+event.preventDefault();
+alert(`Searching for ${Keyword} definition`);
+let apiKey = "eab04436t3405o6e84aadcd05339dfb6"
+let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${Keyword}&key=${apiKey}`
+axios.get(apiUrl).then(handleResponse); 
 }
 
 function handlekeywordChange(event){
