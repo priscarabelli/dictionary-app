328c673cfb6679b716530e20bd9d2219fb1d5b16
priscarabelli <125477480+priscarabelli@users.noreply.github.com>
2023-07-20 11:26:12 +0100

    added h1

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      
+      <h1 className='m-3 text-center'>
+        Dictionary by Pri
+      </h1>
        
       </header>
     </div>
