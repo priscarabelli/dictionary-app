86ff3ae267965ff65a4082dfbefb2b51a963f466
priscarabelli <125477480+priscarabelli@users.noreply.github.com>
2023-07-20 12:51:09 +0100

    added dictionary component and search engine

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 
 import './App.css';
+import Dictionary from "./Dictionary"; 
 
 function App() {
   return (
     <div className="App">
+      <div className='container'>
       <header className="App-header">
       <h1 className='m-3 text-center'>
         Dictionary by Pri
       </h1>
-       
       </header>
+      <main>
+        <Dictionary/>
+      </main>
+      <footer className="App-footer">Coded by Pri Scarabelli</footer>
+      </div>
     </div>
   );
 }
